4e26e4f550d049594087d24d9b1c46072eb2dd68
Maxence Verger <mverger@z3r8p1.42lyon.fr>
2023-04-09 15:46:18 +0200

    fix

--- C01/ex05/src/main.cpp
+++ C01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:21:00 by mverger           #+#    #+#             */
-/*   Updated: 2023/02/24 16:22:20 by mverger          ###   ########.fr       */
+/*   Updated: 2023/04/09 15:45:39 by mverger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main(void) {
     Harl        harl;
 
     std::cout << "Usage: DEBUG, INFO, WARNING or ERROR." << std::endl << std::endl;
-    while (input.compare("exit"))
+    while (input.compare("EXIT"))
     {
         std::cout << "Enter a level: ";
         std::cin >> input;
