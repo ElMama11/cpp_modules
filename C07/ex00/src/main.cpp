45d11afa8a2da6ab97552605a096fe208ce20aa6
Maxence <verger.maxence42@gmail.com>
2023-05-19 16:05:00 +0200

    ex00 done

---
+++ C07/ex00/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 14:39:49 by mverger           #+#    #+#             */
+/*   Updated: 2023/05/19 16:03:18 by mverger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "classes/whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
