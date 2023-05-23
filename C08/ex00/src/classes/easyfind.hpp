38ab5c77b00bc0076dabf45d58c98203979b7e96
Maxence <verger.maxence42@gmail.com>
2023-05-23 16:46:58 +0200

    ex00 done

---
+++ C08/ex00/src/classes/easyfind.hpp
@@ -0,0 +1,16 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <string>
+# include <iostream>
+# include <algorithm>
+# include <list>
+
+template<typename T>
+typename T::iterator easyfind(T &int_cont, int to_find) {
+	typename T::iterator ite;
+	ite = std::find(int_cont.begin(), int_cont.end(), to_find);
+
+	return ite;
+}
+#endif
---
+++ C08/ex00/src/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 14:39:49 by mverger           #+#    #+#             */
+/*   Updated: 2023/05/23 16:29:07 by mverger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "classes/easyfind.hpp"
+
+int main( void ) {
+	std::list<int> lst1;
+
+	lst1.push_back(1);
+	lst1.push_back(5);
+	lst1.push_back(10);
+	lst1.push_back(20);
+	
+	std::list<int>::iterator a =  easyfind(lst1, 10);
+	if (a == lst1.end())
+		std::cout << "Value isn't in the container" << std::endl;
+	else
+		std::cout << *a << std::endl;
+	return 0;
+}
