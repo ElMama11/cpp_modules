0a5296e8306ecc2a187277c186223e5c081e2335
Maxence <verger.maxence42@gmail.com>
2023-05-25 19:38:09 +0200

    ex02 done

---
+++ C08/ex02/src/main.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/06 14:39:49 by mverger           #+#    #+#             */
+/*   Updated: 2023/05/25 19:36:56 by mverger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "classes/MutantStack.hpp"
+
+int main(void)
+{
+    std::cout << "MutantStack : " << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+	
+    MutantStack<int>::container_type::iterator it = mstack.begin();
+    MutantStack<int>::container_type::iterator ite = mstack.end();
+    ++it;
+    --it;
+
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+
+	/*List*/
+
+    std::cout << std::endl;
+    std::cout << "List :" << std::endl;
+
+    std::list<int> lst;
+
+    lst.push_back(5);
+    lst.push_back(17);
+
+    std::cout << lst.back() << std::endl;
+
+    lst.pop_back();
+    std::cout << lst.size() << std::endl;
+
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+
+    std::list<int>::iterator lst_it = lst.begin();
+    std::list<int>::iterator lst_ite = lst.end();
+    ++it;
+    --it;
+    while (lst_it != lst_ite) {
+        std::cout << *lst_it << std::endl;
+        ++lst_it;
+    }
+    std::list<int> l(lst);
+    return 0;
+}
