267446d8fce4f960d60f349bccb5c84d4a49f1c1
Maxence Verger <mverger@z4r9p7.42lyon.fr>
2023-06-02 20:10:38 +0200

    fix main

--- C07/ex00/src/main.cpp
+++ C07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 14:39:49 by mverger           #+#    #+#             */
-/*   Updated: 2023/05/19 16:20:09 by mverger          ###   ########.fr       */
+/*   Updated: 2023/06/02 20:01:54 by mverger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,13 @@ int main( void ) {
 	int a = 2;
 	int b = 3;
 	
-	::swap( a, b );
+	swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	::swap(c, d);
+	swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
--- C07/ex02/src/main.cpp
+++ C07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mverger <mverger@42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 14:39:49 by mverger           #+#    #+#             */
-/*   Updated: 2023/05/19 18:22:51 by mverger          ###   ########.fr       */
+/*   Updated: 2023/06/02 20:09:25 by mverger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 int main(void) {
 	
-	Array<int> a();
-	Array<int> b(3);
-
-	b[0] = 1;
-	b[1] = 2;
-	b[2] = 3;
-	
-	std::cout << b[0] << std::endl;
-	std::cout << b[1] << std::endl;
-	std::cout << b[2] << std::endl;
 
 	try
 	{
+		Array<int> a();
+		Array<int> b(3);
+
+		b[0] = 1;
+		b[1] = 2;
+		b[2] = 3;
+		
+		std::cout << b[0] << std::endl;
+		std::cout << b[1] << std::endl;
+		std::cout << b[2] << std::endl;
 		std::cout << b[3] << std::endl;
 	}
 	catch(const std::exception& e)
